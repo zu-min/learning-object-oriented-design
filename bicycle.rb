b4600307225ac8c1192b452cc18e9e4399ddc419
zu-min <okka.0928@gmail.com>
2017-10-19 17:11:24 +0900

    [2-2]Add Gear Class

--- bicycle.rb
+++ bicycle.rb
@@ -1,10 +1,15 @@
-chainring = 52
-cog       = 11
-ratio     = chainring / cog.to_f
-puts ratio
+class Gear
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog       = cog
+  end
 
-chainring = 30
-cog       = 27
-ratio     = chainring / cog.to_f
-puts ratio
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
 
